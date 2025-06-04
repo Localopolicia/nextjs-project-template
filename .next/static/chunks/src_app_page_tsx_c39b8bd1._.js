046714c471bea1d0d4f5e3a5cbb3e1ba3b7387f3
Localopolicia <150400904+Localopolicia@users.noreply.github.com>
2025-06-04 10:17:16 +0200

    Add .next/static/chunks/src_app_page_tsx_c39b8bd1._.js 1749025037045

---
+++ .next/static/chunks/src_app_page_tsx_c39b8bd1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_da8d969b._.js",
+  "static/chunks/src_37676e57._.js"
+],
+    source: "dynamic"
+});
